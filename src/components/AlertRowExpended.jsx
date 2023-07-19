d93dea3ac4e9a37b2aadd93f4bebe7175a518bdc
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2023-07-19 12:59:28 -0500

    Correción de errores por valores null

--- src/components/AlertRowExpended.jsx
+++ src/components/AlertRowExpended.jsx
@@ -1,14 +1,27 @@
-import {  formatHour } from "../libs/functions"
+import { formatHour } from "../libs/functions"
 import '../styles/AlertRowExpended.scss'
-export const AlertRowExpended = ({alert}) => {
+export const AlertRowExpended = ({ alert }) => {
 
     return (
         <tr className="AlertRowExpended">
             <td colSpan='5'>
-                <p><span>Hora:</span>  {formatHour(alert.createdAt)}</p>
-                <p><span>Componente:</span> {alert.edicion.componente}</p>
-                <p><span>Tipo:</span> {alert.edicion.tipo}</p>
-                <p><span>Por:</span> {alert.usuario.usuario} a las {formatHour(alert.updatedAt)}</p>
+                <p>
+                    <span>Hora:</span>  {formatHour(alert.createdAt)}
+                </p>
+                <p>
+                    <span>Componente:</span> {alert.edicion ? alert.edicion.componente : 'Sin datos'}
+                </p>
+                <p>
+                    <span>Tipo:</span> {alert.edicion ? alert.edicion.tipo : 'Sin datos'}
+                </p>
+                {
+                    alert.usuario && (
+                        <p>
+                            <span>Por: </span>
+                            {alert.usuario?.usuario} a las {formatHour(alert.updatedAt)}</p>
+
+                    )
+                }
             </td>
         </tr>
     )
