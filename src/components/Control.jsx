56d7eacba1373641956db083ae0d26e52f5a1710
Jonathan Valencia <jonathana.valencia@udea.edu.co>
2023-09-25 10:30:27 -0500

    Envio de datos de control con bloque y validaciones

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,55 +1,92 @@
 import { useForm } from 'react-hook-form'
 import '../styles/Control.scss'
+import api from '../api/api'
+import { useState } from 'react'
 export const Control = () => {
 
-    const {register,handleSubmit} = useForm()
+    const [isSaved, setIsSaved] = useState(false);
+    const { register, handleSubmit } = useForm()
 
-    const handleSubmitFormControl = (data) =>{
-        console.log(data)
+    const handleSubmitFormControl = async (data) => {
+        const token = sessionStorage.getItem('token')
+        const bControl = {
+            hora_inicio: data.start_time,
+            hora_final: data.end_time,
+            tiempo_encendido: Number(data.on_time),
+            tiempo_apagado: Number(data.off_time),
+            bloque: parseInt(data.block)
+        }
+
+        const res = await api.post('/control/', bControl, {
+            headers: {
+                Authorization: token
+            }
+        })
+        if (res.status) {
+            setIsSaved(true);
+        }
+
     }
     return (
         <section className="Control">
             <h3>Control</h3>
+            {
+                isSaved && (
+                    <p>Datos almacenados con exito</p>
+                )
+            }
             <form onSubmit={handleSubmit(handleSubmitFormControl)}>
+                <div>
+                    <label htmlFor="block">
+                        Bloque:
+                    </label>
+                    <input
+                        type="number"
+                        id='block'
+                        min='1'
+                        max='20'
+                        required
+                        {...register('block')} />
+                </div>
                 <div>
                     <label htmlFor="startTime">
                         Hora de inicio:
                     </label>
-                    <input 
-                        type="time" 
+                    <input
+                        type="time"
                         id='startTime'
-                        required 
-                        {...register('start_time')}/>
+                        required
+                        {...register('start_time')} />
                 </div>
                 <div>
                     <label htmlFor="endTime">
                         Hora final:
                     </label>
-                    <input 
-                        type="time" 
-                        id='endTime' 
+                    <input
+                        type="time"
+                        id='endTime'
                         required
-                        {...register('end_time')}/>
+                        {...register('end_time')} />
                 </div>
                 <div>
                     <label htmlFor="onTime">
                         Tiempo de encendido:
                     </label>
-                    <input 
-                        type="number" 
-                        id='onTime' 
+                    <input
+                        type="number"
+                        id='onTime'
                         required
-                        {...register('on_time')}/>
+                        {...register('on_time')} />
                 </div>
                 <div>
                     <label htmlFor="offTime">
                         Tiempo de apagado:
                     </label>
-                    <input 
-                        type="number" 
-                        id='offTime' 
+                    <input
+                        type="number"
+                        id='offTime'
                         required
-                        {...register('off_time')}/>
+                        {...register('off_time')} />
                 </div>
                 <button type='submit'>
                     Aplicar
